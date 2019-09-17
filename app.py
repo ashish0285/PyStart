77d82200ec190cbc45303cc7fed1f80f6f1cda69
Ashish Mishra <ashish.mishra@yahoo.co.in>
2019-09-17 17:59:44 +0530

    Variables & data types  added

--- app.py
+++ app.py
@@ -117,3 +117,37 @@ if ( a is not b ):
  print ("Line 5 - a and b do not have same identity")
 else:
  print ("Line 5 - a and b have same identity")
+
+
+# precedence operators
+
+""" ** Exponentiation (raise to the power)
+~ + - Ccomplement, unary plus and minus (method names for
+the last two are +@ and -@)
+* / % // Multiply, divide, modulo and floor division
++ - Addition and subtraction
+>> << Right and left bitwise shift
+& Bitwise 'AND'
+^ | Bitwise exclusive `OR' and regular `OR'
+<= < > >= Comparison operators
+<> == != Equality operators
+= %= /= //= -= += *=
+**=
+Assignment operators
+is is not Identity operators
+in not in Membership operators
+not or and Logical operators """
+
+a = 20
+b = 10
+c = 15
+d = 5
+print ("a:%d b:%d c:%d d:%d" % (a,b,c,d ))
+e = (a + b) * c / d #( 30 * 15 ) / 5
+print ("Value of (a + b) * c / d is ", e)
+e = ((a + b) * c) / d # (30 * 15 ) / 5
+print ("Value of ((a + b) * c) / d is ", e)
+e = (a + b) * (c / d) # (30) * (15/5)
+print ("Value of (a + b) * (c / d) is ", e)
+e = a + (b * c) / d # 20 + (150/5)
+print ("Value of a + (b * c) / d is ", e)
