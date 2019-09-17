8b75027b17a5ee0a8ea384f0c537e5d5bb6a29ad
Ashish Mishra <ashish.mishra@yahoo.co.in>
2019-09-17 17:53:20 +0530

    Variables & data types  added

--- app.py
+++ app.py
@@ -1,46 +1,119 @@
-#!/usr/bin/python3
-import sys, getopt
-def main(argv):
- inputfile = ''
- outputfile = ''
- try:
-    opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
- except getopt.GetoptError:
-    print ('app.py -i <inputfile> -o <outputfile>')
-    sys.exit(2)
- for opt, arg in opts:
-    if opt == '-h':
-        print ('app.py -i <inputfile> -o <outputfile>')
-        sys.exit()
-    elif opt in ("-i", "--ifile"):
-        inputfile = arg
-    elif opt in ("-o", "--ofile"):
-        outputfile = arg
- print ('Input file is "', inputfile)
- print ('Output file is "', outputfile)
-if __name__ == "__main__":
- main(sys.argv[1:])
-"""  getopt.getopt(args, options, [long_options]) 
-Here is the detail of the parameters-
- args: This is the argument list to be parsed.
- options: This is the string of option letters that the script wants to recognize, with
-options that require an argument should be followed by a colon (:).
- long_options: This is an optional parameter and if specified, must be a list of
-strings with the names of the long options, which should be supported. Long
-options, which require an argument should be followed by an equal sign ('='). To
-accept only long options, options should be an empty string.
- This method returns a value consisting of two elements- the first is a list
-of (option, value) pairs, the second is a list of program arguments left after the
-option list was stripped. 
-Each option-and-value pair returned has the option as its first element, prefixed
-with a hyphen for short options (e.g., '-x') or two hyphens for long options (e.g., '-
--long-option').
-usage: app.py -i <inputfile> -o <outputfile>
-$ app.py -h
-usage: app.py -i <inputfile> -o <outputfile>
-$ app.py -i BMP -o
-usage: app.py -i <inputfile> -o <outputfile>
-$ app.py -i inputfile -o outputfile
-Input file is " inputfile
-Output file is " outputfile
-"""
+counter = 100 # An integer assignment
+miles = 1000.0 # A floating point
+name = "John" # A string
+print (counter)
+print (miles)
+print (name)
+
+str = 'Hello World!'
+print (str) # Prints complete string
+print (str[0]) # Prints first character of the string
+print (str[2:5]) # Prints characters starting from 3rd to 5th
+print (str[2:]) # Prints string starting from 3rd character
+print (str * 2) # Prints string two times
+print (str + "TEST") # Prints concatenated string
+
+list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+tinylist = [123, 'john']
+print (list) # Prints complete list
+print (list[0]) # Prints first element of the list
+print (list[1:3]) # Prints elements starting from 2nd till 3rd
+print (list[2:]) # Prints elements starting from 3rd element
+print (tinylist * 2) # Prints list two times
+print (list + tinylist) # Prints concatenated lists
+
+""" Python Tuples
+A tuple is another sequence data type that is similar to the list. A tuple consists of a
+number of values separated by commas. Unlike lists, however, tuples are enclosed within
+parenthesis.
+The main difference between lists and tuples is- Lists are enclosed in brackets ( [ ] ) and
+their elements and size can be changed, while tuples are enclosed in parentheses ( ( ) )
+and cannot be updated. Tuples can be thought of as read-only lists.  """
+
+tuple = ( 'abcd', 786 , 2.23, 'john', 70.2 )
+tinytuple = (123, 'john')
+print (tuple) # Prints complete tuple
+print (tuple[0]) # Prints first element of the tuple
+print (tuple[1:3]) # Prints elements starting from 2nd till 3rd
+print (tuple[2:]) # Prints elements starting from 3rd element
+print (tinytuple * 2) # Prints tuple two times
+print (tuple + tinytuple) # Prints concatenated tuple
+
+""" 
+Python Dictionary
+Python's dictionaries are kind of hash-table type. They work like associative arrays or
+hashes found in Perl and consist of key-value pairs. A dictionary key can be almost any
+Python type, but are usually numbers or strings. Values, on the other hand, can be any
+arbitrary Python object. """
+
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+print (dict['one']) # Prints value for 'one' key
+print (dict[2]) # Prints value for 2 key
+print(dict)  # Prints the dictionary
+print (tinydict) # Prints complete dictionary
+print (tinydict.keys()) # Prints all the keys
+print (tinydict.values()) # Prints all the values
+
+""" Data Type Conversion
+int(x [,base]) Converts x to an integer. The base specifies the base if x is a string.
+float(x) Converts x to a floating-point number.
+complex(real [,imag]) Creates a complex number.
+str(x) Converts object x to a string representation.
+repr(x) Converts object x to an expression string.
+eval(str) Evaluates a string and returns an object.
+tuple(s) Converts s to a tuple.
+list(s) Converts s to a list.
+set(s) Converts s to a set.
+dict(d) Creates a dictionary. d must be a sequence of (key,value) tuples.
+frozenset(s) Converts s to a frozen set.
+chr(x) Converts an integer to a character.
+unichr(x) Converts an integer to a Unicode character.
+ord(x) Converts a single character to its integer value.
+hex(x) Converts an integer to a hexadecimal string.
+oct(x) Converts an integer to an octal string. """
+
+
+
+a = 21
+b = 10
+c = 0
+c = a + b
+print ("Line 1 - Value of c is ", c)
+c = a - b
+print ("Line 2 - Value of c is ", c )
+c = a * b
+print ("Line 3 - Value of c is ", c)
+c = a / b
+print ("Line 4 - Value of c is ", c )
+c = a % b
+print ("Line 5 - Value of c is ", c)
+a = 2
+b = 3
+c = a**b # exponent
+print ("Line 6 - Value of c is ", c)
+a = 10
+b = 5
+c = a//b #floor division (div)
+print ("Line 7 - Value of c is ", c)
+
+
+a = 20
+b = 20
+print ('Line 1','a=',a,':',id(a), 'b=',b,':',id(b))
+if ( a is b ):
+ print ("Line 2 - a and b have same identity")
+else:
+ print ("Line 2 - a and b do not have same identity")
+if ( id(a) == id(b) ):
+ print ("Line 3 - a and b have same identity")
+else:
+ print ("Line 3 - a and b do not have same identity")
+b = 30
+print ('Line 4','a=',a,':',id(a), 'b=',b,':',id(b))
+if ( a is not b ):
+ print ("Line 5 - a and b do not have same identity")
+else:
+ print ("Line 5 - a and b have same identity")
