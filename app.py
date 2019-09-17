a8d2d296f2a3504a179c0d0b3621cb6021eb773e
Ashish Mishra <ashish.mishra@yahoo.co.in>
2019-09-17 15:11:08 +0530

    some basic additions

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-print('Hello World.. Ashish')
+print('Hello World.. Ashish', end=" ")
 print('Ashish-' * 10)
-
+x=input('something : ')
+print(' Input something is '+ x)
+print(3/2)
+if True:
+    print('Printing True')
+else:
+    print('Not Printed')
 
