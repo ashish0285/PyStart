fac918bb62676a8c0647fe0fc9b35a777bcf1f44
Ashish Mishra <ashish.mishra@yahoo.co.in>
2019-09-17 15:18:30 +0530

    adding command line args

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-print('Hello World.. Ashish', end=" ")
-print('Ashish-' * 10)
-x=input('something : ')
-print(' Input something is '+ x)
-print(3/2)
-if True:
-    print('Printing True')
-else:
-    print('Not Printed')
-
+import sys
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+print ('Argument List:', str(sys.argv))
